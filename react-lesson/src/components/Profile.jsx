bd44120fe89088abc8f44609621ac55084652f04
antonukD <91678347+antonukD@users.noreply.github.com>
2021-11-04 15:02:38 +0200

    lesson13
    lesson13

---
+++ react-lesson/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+<div className='content'>
+        <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+        <div> <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM--Uugl9NzBLmqo76weB6LKmcG6PLq-aXdg&usqp=CAU' /></div>
+        <div>ava + description</div>
+        <div>My posts</div>
+        <div>New post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+</div>
+
+
+}
+
+export default Profile; 
